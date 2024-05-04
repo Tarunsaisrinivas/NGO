335bf943e4a86a4f886dd79e0a4970efcaac7c0c
Tarun Sai Srinivas <tarunsaisrinivas7@gmail.com>
2024-05-04 11:09:19 +0530

    first

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb://localhost:27017/your_database', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://tarun:tarunsai2341@cluster0.tbd0fbb.mongodb.net/madhuri?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error(err));
 
@@ -27,6 +27,7 @@ app.use(bodyParser.json());
 
 // API endpoint for saving data to the database
 app.post('/api/data', async (req, res) => {
+ 
   try {
     const { title, description, dropdownSelection } = req.body;
     const newData = new DataModel({ title, description, dropdownSelection });

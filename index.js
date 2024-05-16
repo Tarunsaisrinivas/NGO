f701f6f77d58ac8454cf7227d99678eebbe90d35
Tarun Sai Srinivas <92567478+Tarunsaisrinivas@users.noreply.github.com>
2024-05-16 20:11:38 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB
-mongoose.connect('mongodb+srv://tarun:tarunsai2341@cluster0.tbd0fbb.mongodb.net/madhuri?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
 
